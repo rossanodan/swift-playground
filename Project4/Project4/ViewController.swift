740ed9669bd1cfa46980e7d399d4ccfe1df71071
Rossano D'Angelo <rossanodan@gmail.com>
2020-12-08 23:33:31 +0000

    Started working on Project4 - TODO Choosing a website: UIAlertController action sheets

---
+++ Project4/Project4/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Rossano D’Angelo on 12/8/20.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+
+
+}
+
